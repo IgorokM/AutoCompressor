b915d11bf789f0480f056f59699aa2c5767b3845
IgorM <igorokiscool@gmail.com>
2019-12-25 15:27:35 +0300

    refactoring

--- index.js
+++ index.js
@@ -18,20 +18,22 @@ function run(root) {
     const insideRoot = fs.readdirSync(root);
     const stringInsideRoot = insideRoot.join();
     const hasArchive = stringInsideRoot.search(/.gz?/);
-    console.log(`Scanning ${root}`);
-    if (!Array.isArray(insideRoot)) return false;
-
-    for (let item of insideRoot) {
-        let insidePath = path.join(root, item);
-        let stat = fs.statSync(insidePath);
-        console.log(stat);
-        if (stat.isFile()) {
-            packZip(insidePath);
-            return true;
-        } else if(stat.isDirectory()) {
-            run(insidePath);
+    if (hasArchive === -1) {
+        console.log(`Scanning ${root}`);
+        for (let item of insideRoot) {
+            let insidePath = path.join(root, item);
+            let stat = fs.statSync(insidePath);
+            if (stat.isFile()) {
+                packZip(insidePath);
+                return true;
+            } else if (stat.isDirectory()) {
+                run(insidePath);
+            }
         }
+    }else{
+        console.log(`in folder '${root}' not work`);
     }
+    return true;
 }
 function packZip(pathToFile) {
     const r = fs.createReadStream(pathToFile);
